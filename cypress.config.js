829f508b6d4fd96bd1bdfef9e8c265d8b455cbc6
tifannyy <fannynovelia9@gmail.com>
2025-01-18 11:42:13 +0700

    update cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,18 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://magento.softwaretestingboard.com/',
+    urlProd: 'www.prod.com',
+    env:{
+      userstg: "user stg",
+      userprod: "user prod"
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    viewportHeight: 650,
+    viewportWidth: 950,
+    defaultCommandTimeout: 4500,
+    screenshotOnRunFailure: false
   },
 });
