551c187c6a824a9f64b3210d71c4c374f32e64f3
gerimaulana <gerimaulana21@gmail.com>
2025-01-19 21:42:45 +0700

    removing other project

--- cypress/support/element-pages/detailPage.js
+++
@@ -1,9 +0,0 @@
-class detailPage {
-    size_picker = '#option-label-size-143-item-168'
-    color_picker = '#option-label-color-93-item-56'
-    addtocart_btn = '#product-addtocart-button'
-    cart = '[data-block="minicart"]'
-    validatecheckout_btn = '[data-action="close"]'
-    proceedtocheckout_btn = 'button[id="top-cart-btn-checkout"]'
-}
-module.exports = new detailPage()
