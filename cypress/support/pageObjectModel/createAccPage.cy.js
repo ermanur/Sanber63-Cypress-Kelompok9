d1996e24b80bf0f7c3d0e34921181d2c11cebb32
Gerimaulana <113164707+Gerimaulana@users.noreply.github.com>
2025-01-19 21:49:28 +0700

    Delete cypress/support/pageObjectModel/createAccPage.cy.js

--- cypress/support/pageObjectModel/createAccPage.cy.js
+++
@@ -1,9 +0,0 @@
-class createAccPage {
-    firstname = '#firstname'
-    lastname = '#lastname'
-    email = '#email_address'
-    password = '#password'
-    confirm_pass = '#password-confirmation'
-}
-
-module.exports = new createAccPage()
